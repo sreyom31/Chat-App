75d488b61ab9e78e8f47646d161e8099bc12d766
sreyom31 <sreyom31@gmail.com>
2021-11-14 07:19:52 +0530

    socketIo events

--- src/index.js
+++ src/index.js
@@ -12,8 +12,14 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
-    console.log('New Websocket Connection');
+io.on('connection', (socket) => {
+    console.log('New Websocket connection');
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
 })
 
 server.listen(port, () => {
