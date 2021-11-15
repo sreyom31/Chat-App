0b80820cdda447151aee5349bbf5db9397e8711c
sreyom31 <sreyom31@gmail.com>
2021-11-15 13:47:05 +0530

    Sending messages to rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,6 +18,7 @@ const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true }
 socket.on('message', (message) => {
     console.log(message);
     const html = Mustache.render(messageTemplate, {
+        username: 'Admin',
         message: message.text,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
@@ -27,6 +28,7 @@ socket.on('message', (message) => {
 socket.on('locationMessage', message => {
     console.log(message);
     const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
         url: message.url,
         createdAt: moment(message.createdAt).format('h:mm a')
     })
